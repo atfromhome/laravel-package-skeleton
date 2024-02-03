13a1c4a63658614d2244e872297094318fc30cb9
Nuradiyana <me@nooradiana.com>
2024-02-03 22:19:37 +0700

    Create SkeletonTest.php

---
+++ tests/SkeletonTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VendorName\Skeleton\Tests;
+
+final class SkeletonTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
